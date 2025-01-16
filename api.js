08a721c6a4a9f919271895d8992ef6005ed08a83
rixcx <adts0415.ovo@gmail.com>
2025-01-16 10:56:05 +0900

    [index/sever] add POST function

--- api.js
+++ api.js
@@ -1,9 +1,9 @@
 const URL = 'http://localhost:3000';
-const API_URL = `${URL}/todos`;
+const API_URL = `${URL}/todos`; //HACK: envに移すべき？
 
 // Todoのfetch
 async function fetchTodos() {
-  const response = await fetch(API_URL); //※取得できなかった場合のエラー追加
+  const response = await fetch(API_URL); //TODO:取得できなかった場合のエラー
   const todos = await response.json();
   
   const todoList = document.getElementById('js-todolists');
@@ -13,6 +13,29 @@ async function fetchTodos() {
     todoList.appendChild(liItem);
   });
 }
+
+// Todo post
+async function addTodo() {
+  const todoInput = document.getElementById('js-newtodo');
+  const newTodo = todoInput.value.trim();
+  console.log(newTodo);
+  
+  if (!newTodo) {
+    alert('content is required');
+    return;
+  }
+  
+  // POSTするAPI
+  await fetch(API_URL, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' }, //jsonで送ることを宣言
+    body: JSON.stringify({ todo: newTodo }) //送れるのは文字列だけなので、文字列にする
+  });
+  todoInput.value = '';
+}
+  // クリック後発火
+ document.getElementById('js-addtodo').addEventListener('click', addTodo);
+
 
 // ロード後発火
 window.onload = fetchTodos;
--- server.js
+++ server.js
@@ -28,10 +28,24 @@ app.use(cors());
 app.use(express.json());
 
 // エンドポイント
+// GET 一覧表示
 app.get('/todos', (req, res) => {
   const todos = loadTodos();
   res.json(todos);
 });
+
+// POST todo追加
+app.post('/todos', (req, res) => {
+  const todos = loadTodos();
+  const newTodo = req.body.todo; // キー名 'todo' を使って値を取得
+  if (!newTodo) {
+    return res.status(400).json({ error: 'Task content is required' });
+  }
+  todos.push(newTodo);
+  saveTodos(todos);
+  res.status(201).json({ message: 'Task added', todo: newTodo });
+});
+
 
 // サーバーを起動
 app.listen(3000, () => {
